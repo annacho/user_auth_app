1ff955d1f258f11a3751887a79ab7c8ef01f1573
Anna Cho <annacho13@gmail.com>
2014-05-12 15:31:52 -0400

    init

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "login" => "sessions#new"
   get "logout" => "sessions#destroy"
   resources :users
-  resources :sessions, only [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
